198c1f396a188566111d5be93fd777fd520cf769
Michael Sproul <micsproul@gmail.com>
2015-09-23 12:36:49 +1000

    Reduction rules 1 and 2 for compression alg.

--- fvs.py
+++ fvs.py
@@ -1,1 +1,29 @@
 import networkx as nx
+
+from networkx import MultiGraph
+from networkx.algorithms.tree import is_forest
+
+def is_fvs(g: MultiGraph, w) -> bool:
+	gx = g.copy()
+	gx.remove_nodes_from(w)
+	return is_forest(gx)
+
+def reduction1(g: MultiGraph) -> MultiGraph:
+	gx = g.copy()
+	for v in gx.nodes():
+		if gx.degree(v) <= 1:
+			gx.remove_node(v)
+	return gx
+
+def reduction2(g: MultiGraph, w: set, k: int) -> (MultiGraph, set, int):
+	gx = g.copy()
+	for v in gx.nodes():
+		wv = w.copy()
+		wv.add(v)
+		if is_forest(gx.subgraph(wv)):
+			gx.remove_node(v)
+			return (gx, w, k - 1)
+	return (gx, w, k)
+
+def fvs_compression(g: MultiGraph, k: int) -> bool:
+	return False
