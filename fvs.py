4bc084d8d7030201ab432cf69efee8064cecbca4
Michael Sproul <micsproul@gmail.com>
2015-09-23 10:39:54 +1000

    Add __repr__.

--- fvs.py
+++ fvs.py
@@ -4,12 +4,15 @@ class Graph():
 	def __init__(self):
 		self.v = {}
 
+	def __repr__(self):
+		return "Graph " + self.v.__repr__()
+
 	def add_vertex(self, vertex):
 		if not vertex in self.v:
 			self.v[vertex] = set()
 		else:
 			print("Warning: adding already existing vertex")
-	
+
 	def remove_vertex(self, vertex):
 		if vertex in self.v:
 			for v2 in self.v[vertex]:
@@ -17,7 +20,7 @@ class Graph():
 			del self.v[vertex]
 		else:
 			print("Warning: removing non-existing vertex")
-	
+
 	def remove_vertex_neighbourhood(self, vertex):
 		if self.contains_vertex(vertex):
 			for v2 in self.v[vertex]:
@@ -35,7 +38,7 @@ class Graph():
 				print("Warning: adding edge from vertex to self")
 		else:
 			print("Warning: adding edge between non-existant vertices")
-	
+
 	def remove_edge(self, v_from, v_to):
 		if self.contains_vertex(v_from) and self.contains_vertex(v_to):
 			if self.contains_edge(v_from, v_to):
@@ -45,7 +48,7 @@ class Graph():
 				print("Warning: removing non-existant edge")
 		else:
 			print("Warning: removing edge from non-existant vertices")
-	
+
 	def contains_vertex(self, vertex):
 		return vertex in self.v
 
